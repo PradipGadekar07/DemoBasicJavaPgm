e2572efb9f7a0ca2ac4d46052b101de8b1291d4b
Pradip Gadekar <Pradip Gadekar@Sonali-PC.mshome.net>
2017-09-07 10:50:15 +0530

    serialization pgm

--- SCOOPEN/src/demopgm/SerealizationTest.java
+++ SCOOPEN/src/demopgm/SerealizationTest.java
@@ -19,7 +19,6 @@ class Demo implements java.io.Serializable {
 	}
 
 }
-
 class SerealizationTest {
 	public static void main(String[] args) {
 		Demo object = new Demo(7, "MS Dhoni");
@@ -30,15 +29,11 @@ class SerealizationTest {
 			// Saving of object in a file
 			FileOutputStream file = new FileOutputStream(filename);
 			ObjectOutputStream out = new ObjectOutputStream(file);
-
 			// Method for serialization of object
 			out.writeObject(object);
-
 			out.close();
 			file.close();
-
 			System.out.println("Object has been serialized");
-
 		}
 
 		catch (IOException ex) {
@@ -67,10 +62,8 @@ class SerealizationTest {
 		catch (IOException ex) {
 			System.out.println("IOException is caught");
 		}
-
 		catch (ClassNotFoundException ex) {
 			System.out.println("ClassNotFoundException is caught");
 		}
-
 	}
 }
